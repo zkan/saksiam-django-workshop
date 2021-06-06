042f27cb233356a8e30a5fe80f73625c337169e8
zkan <zkan.cs@gmail.com>
2021-06-06 22:43:29 +0700

    Create my profile index page :bear:

--- my_profile/core/views.py
+++ my_profile/core/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    html = """
+      <html>
+        <head>
+          <title>Kan Ouivirach</title>
+        </head>
+        <body>
+          <h1>Kan Ouivirach</h1>
+          <p>Data Craftsman. Passionate in software engineering, data engineering, and data science.</p>
+          <div><span>GitHub:</span> https://github.com/zkan/</div>
+          <div><span>Facebook:</span> https://www.facebook.com/zkan.cs/</div>
+          <div><span>Twitter:</span> https://twitter.com/zkancs</div>
+        </body>
+      </html>
+    """
+    return HttpResponse(html)
