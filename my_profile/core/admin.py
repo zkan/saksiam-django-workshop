8774e050f9a98af3fe97a8b0f2d1176a5ad7019c
zkan <zkan.cs@gmail.com>
2021-06-07 22:13:11 +0700

    Register profile model to admin :bear:

--- my_profile/core/admin.py
+++ my_profile/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from core.models import Profile
+
+
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+    )
+    search_fields = (
+        "name",
+    )
