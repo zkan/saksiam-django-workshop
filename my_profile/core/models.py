6045a10d2f28614e51a49a807613f91787eddfbb
zkan <zkan.cs@gmail.com>
2021-06-08 17:53:52 +0700

    Change field to URLField (more appropriate) :bear:

--- my_profile/core/models.py
+++ my_profile/core/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Profile(models.Model):
     name = models.CharField(max_length=300)
     short_bio = models.TextField()
-    github_url = models.CharField(max_length=300)
-    facebook_url = models.CharField(max_length=300)
-    twitter_url = models.CharField(max_length=300)
+    github_url = models.URLField(max_length=300)
+    facebook_url = models.URLField(max_length=300)
+    twitter_url = models.URLField(max_length=300)
 
 
 class Subscriber(models.Model):
