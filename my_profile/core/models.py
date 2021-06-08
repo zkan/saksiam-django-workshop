281324c318fad086dbcd39cbcacd7bb2ec998bb3
zkan <zkan.cs@gmail.com>
2021-06-08 17:41:36 +0700

    Complete my profile model :bear:

--- my_profile/core/models.py
+++ my_profile/core/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class Profile(models.Model):
     name = models.CharField(max_length=300)
+    short_bio = models.TextField()
+    github_url = models.CharField(max_length=300)
+    facebook_url = models.CharField(max_length=300)
+    twitter_url = models.CharField(max_length=300)
