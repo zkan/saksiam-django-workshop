049215da37b256ffc91f3a8502c61b6bf2ebfbf8
zkan <zkan.cs@gmail.com>
2021-06-08 22:11:25 +0700

    Add show profile command :bear:

---
+++ my_profile/core/management/commands/show_profile.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from core.models import Profile
+
+
+class Command(BaseCommand):
+    help = 'This is a command to show my profile!'
+
+    def add_arguments(self, parser):
+        parser.add_argument('id', nargs=1, type=int)
+        parser.add_argument('name', nargs=1, type=str)
+
+    def handle(self, *args, **options):
+        print(args)
+        print(options)
+
+        try:
+            id = options.get("id")[0]
+            print(id)
+            profile = Profile.objects.get(id=id)
+            print(profile.name)
+            print(profile.short_bio)
+        except Exception:
+            raise CommandError("Error!")
